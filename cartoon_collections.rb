214bba7b2e50c8f4391672e95b50d6c62a54a496
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-04 13:16:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index do { |item, index|
+    array[item] = index
+  }
   # Your code here
+  array
 end
 
 def summon_captain_planet# code an argument here
