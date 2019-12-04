09f60affc18547923a46f0af08136c523692b371
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-04 13:48:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,14 +16,19 @@ def long_planeteer_calls(array)
 end
 
 
+# def find_the_cheese(array)
+#   if array.include?("cheddar")
+#     return "cheddar"
+#   elsif array.include?("gouda")
+#     return "gouda"
+#   elsif array.include?("camembert")
+#     return "camembert"
+#   else
+#     nil
+#   end
+# end
 def find_the_cheese(array)
-  if array.include?("cheddar")
-    return "cheddar"
-  elsif array.include?("gouda")
-    return "gouda"
-  elsif array.include?("camembert")
-    return "camembert"
-  else
-    nil
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
 end
